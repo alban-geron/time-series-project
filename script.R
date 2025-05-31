918526d4fa41a34e6c9007a388c7a548fc58a7eb
Alban Géron <albangeron@yahoo.fr>
2025-05-31 11:29:14 +0200

    Part I

--- script.R
+++ script.R
@@ -8,46 +8,33 @@ library(ggplot2)
 library(forecast)
 library(patchwork)
 library(dplyr)
+library(lmtest)
 
+# Processing data
 raw <- read_delim(
   "data.csv",
   delim = ";",
   col_names = FALSE,
   show_col_types = FALSE
 )
-
-
-
-# --- Question 2 ---
-
 raw_month <- raw[ grepl("^\\d{4}-\\d{2}$", raw$X1) , ]
-min_date <- ymd(paste0(tail(raw_month$X1, 1), "-01"))
+min_date <- ymd(paste0(tail(raw_month$X1, 1), "-01")) # 1990-01-01
 y <- ts(
   rev(as.numeric(raw_month$X2)),
   start = c(year(min_date), month(min_date)),
   frequency = 12
 )
 
-# Test ADF
+plot(y)
+
+# Testing stationarity
 adf.test(y)
+pp.test(y)
+kpss.test(y)
 
-# First difference (d = 1)
-y_diff  <- diff(y)
-
-# Seasonal difference (D = 1, period 12)
-y_diffS <- diff(y, lag = 12)
-
-# Both d = 1 et D = 1
-y_stab <- diff(y_diffS)
-
-cat("\nADF after first difference: p =", adf.test(y_diff)$p.value)
-cat("\nADF after seasonal difference: p =", adf.test(y_diffS)$p.value)
-cat("\nADF after d = 1 & D = 1 (both diffs): p =", adf.test(y_stab)$p.value, "\n")
-
-
-
-# --- Question 3 ---
-
-p1 <- autoplot(y) + ggtitle("Before differencing") # --> série clairement non stationnaire !!! contrairement à ce que dit ADF
-p2 <- autoplot(y_stab) + ggtitle("After differencing")
-p1 + p2
+# Testing stationarity on first-order differenced series
+y_diff <- diff(y)
+plot(y_diff)
+adf.test(y_diff)
+pp.test(y_diff)
+kpss.test(y_diff)
